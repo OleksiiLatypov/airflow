377f80c969c982a331f23d56984eff375cecc9a1
oleksiilatypov <latypov.oleksii.la@gmail.com>
2025-06-13 00:20:21 +0300

    parsing

--- dags/main.py
+++ dags/main.py
@@ -9,7 +9,6 @@ headers = {
     'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 }
 
-
 base_url = f"https://www.amazon.com/s?k=data+engineering+books"
 check = requests.get(base_url, headers=headers)
 print(check)
@@ -21,9 +20,11 @@ for i in range(1):
     url = f'{base_url}&page={i}'
     response = requests.get(url, headers=headers)
     print(response)
-    #print(requests.get(page, headers=headers))
+    # print(requests.get(page, headers=headers))
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        authors = soup.find_all('a', {'class': 'a-size-base a-link-normal s-underline-text s-underline-link-text s-link-style'})
-        print(authors)
+        authors = author = soup.find('a',
+                                     class_='a-size-base a-link-normal s-underline-text s-underline-link-text s-link-style')
 
+        print(authors)
+        print(authors.text)
